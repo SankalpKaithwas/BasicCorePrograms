de0c10ef7eb4ee1e5995c54bbea0f695d9184d84
unknown <kaithwas.697@gmail.com>
2022-03-14 18:31:08 +0530

    Program to check a year is leap year or not

---
+++ Basic Core Programs/Basic Core Programs/LeapYear.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Basic_Core_Programs
+{
+    internal class LeapYear
+    {
+        public void CheckLeapYear(int year)
+        {
+            int count = 0;
+            int temp = 1;
+            while (temp <= year)
+            {
+                temp *= 10;
+                count++;
+            }
+            if (count == 4)
+            {
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+                {
+                    Console.WriteLine($"{year} is a leap year");
+                }
+                else
+                {
+                    Console.WriteLine($"{year} is not a leap year");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter a proper year");
+            }
+        }
+    }
+}
--- Basic Core Programs/Basic Core Programs/Program.cs
+++ Basic Core Programs/Basic Core Programs/Program.cs
@@ -7,6 +7,7 @@ namespace Basic_Core_Programs
         static void Main(string[] args)
         {
             Console.WriteLine("Choose 1 for Flipcoin");
+            Console.WriteLine("Choose 2 for Leap year");
             int option = int.Parse(Console.ReadLine());
 
             switch (option)
@@ -16,6 +17,11 @@ namespace Basic_Core_Programs
                     Console.WriteLine("Enter numbers of time coin needs to flip");
                     int input = int.Parse(Console.ReadLine());
                     percentage.FlipCoinPercentage(input);
+                    break;
+                case 2: LeapYear checkLeapYear = new LeapYear();
+                     Console.WriteLine("Enter year to check if it is leap year or not");
+                    int year = int.Parse(Console.ReadLine());                    
+                    checkLeapYear.CheckLeapYear(year);
                     break;
                 default:
                     Console.WriteLine("Input valid option");
