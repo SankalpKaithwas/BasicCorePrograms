4a8952561eb49d0a045b8e55d6095fddaa274871
unknown <kaithwas.697@gmail.com>
2022-03-14 18:22:46 +0530

    Program to find percentage of head and tails for given flips

---
+++ Basic Core Programs/Basic Core Programs/FlipCoin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Basic_Core_Programs
+{
+    internal class FlipCoin
+    {
+        public void FlipCoinPercentage(int number)
+        {
+            int headCount = 0;
+            int tailCount = 0;
+            if (number > 0)
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    int checkToss = new Random().Next(0, 2);
+                    if (checkToss == 0)
+                    {
+                        headCount++;
+                    }
+                    else
+                    {
+                        tailCount++;
+                    }
+                }
+                double headPercent = (double)headCount / number * 100;
+                double tailPercent = (double)tailCount / number * 100;
+                Console.WriteLine("Head percentage is -> " + Math.Round(headPercent, 1));
+                Console.WriteLine("Tail percentage is -> " + Math.Round(tailPercent, 1));
+            }
+            else
+            {
+                Console.WriteLine("Enter a Positive integer");
+            }
+        }
+    }
+}
---
+++ Basic Core Programs/Basic Core Programs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Basic_Core_Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose 1 for Flipcoin");
+            int option = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    FlipCoin percentage = new FlipCoin();
+                    Console.WriteLine("Enter numbers of time coin needs to flip");
+                    int input = int.Parse(Console.ReadLine());
+                    percentage.FlipCoinPercentage(input);
+                    break;
+                default:
+                    Console.WriteLine("Input valid option");
+                    break;
+            }
+        }
+    }
+}
