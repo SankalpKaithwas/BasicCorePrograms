67b280d68d007f5b0cafa6f64e174cc3d0a2d907
unknown <kaithwas.697@gmail.com>
2022-03-14 19:15:52 +0530

    Added feature to get prime factors of a number

---
+++ Basic Core Programs/Basic Core Programs/PrimeFactor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Basic_Core_Programs
+{
+    internal class PrimeFactor
+    {
+
+        public void PrimeFactors(int number)
+        {
+
+            Console.WriteLine($"Factors of {number} are: ");
+            for (int i = 1; i * i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    if (isPrime(i))
+                    {
+                        Console.WriteLine(i);
+                    }
+                    else { Console.WriteLine("No factors must be prime number"); }
+                }
+            }
+
+
+        }
+        public bool isPrime(int number)
+        {
+            int count = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- Basic Core Programs/Basic Core Programs/Program.cs
+++ Basic Core Programs/Basic Core Programs/Program.cs
@@ -10,6 +10,7 @@ namespace Basic_Core_Programs
             Console.WriteLine("Choose 2 for Leap year");
             Console.WriteLine("Choose 3 for Power of 2");
             Console.WriteLine("Choose 4 for Harmonic number");
+            Console.WriteLine("Choose 5 for Prime factors");
             int option = int.Parse(Console.ReadLine());
 
             switch (option)
@@ -37,6 +38,12 @@ namespace Basic_Core_Programs
                     Console.WriteLine("Enter the number of harmonic ");
                     int term = int.Parse(Console.ReadLine());
                     harmonic.PrintHarmonic(term);
+                    break;
+                case 5:
+                    PrimeFactor primeFactor = new PrimeFactor();
+                    Console.WriteLine("Enter the number ");
+                    int number = int.Parse(Console.ReadLine());
+                    primeFactor.PrimeFactors(number);
                     break;
                 default:
                     Console.WriteLine("Input valid option");
