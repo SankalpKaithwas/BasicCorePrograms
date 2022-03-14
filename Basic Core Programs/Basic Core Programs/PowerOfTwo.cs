976f670772661e004bde0f0b6ebcf9aa8076fa68
unknown <kaithwas.697@gmail.com>
2022-03-14 18:39:21 +0530

    Added feature to get number upto power of 2

---
+++ Basic Core Programs/Basic Core Programs/PowerOfTwo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Basic_Core_Programs
+{
+    internal class PowerOfTwo
+    {
+        public void CheckPowerOfTwo(int power)
+        {
+            double result = 0;
+            double condition = Math.Pow(2, power);
+            for (int i = 1; result <= condition; i++)
+            {
+                result = Math.Pow(2, i);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
--- Basic Core Programs/Basic Core Programs/Program.cs
+++ Basic Core Programs/Basic Core Programs/Program.cs
@@ -8,6 +8,7 @@ namespace Basic_Core_Programs
         {
             Console.WriteLine("Choose 1 for Flipcoin");
             Console.WriteLine("Choose 2 for Leap year");
+            Console.WriteLine("Choose 3 for Power of 2");
             int option = int.Parse(Console.ReadLine());
 
             switch (option)
@@ -18,10 +19,17 @@ namespace Basic_Core_Programs
                     int input = int.Parse(Console.ReadLine());
                     percentage.FlipCoinPercentage(input);
                     break;
-                case 2: LeapYear checkLeapYear = new LeapYear();
-                     Console.WriteLine("Enter year to check if it is leap year or not");
-                    int year = int.Parse(Console.ReadLine());                    
+                case 2:
+                    LeapYear checkLeapYear = new LeapYear();
+                    Console.WriteLine("Enter year to check if it is leap year or not");
+                    int year = int.Parse(Console.ReadLine());
                     checkLeapYear.CheckLeapYear(year);
+                    break;
+                case 3:
+                    PowerOfTwo powerOfTwo = new PowerOfTwo();
+                    Console.WriteLine("Enter power of 2 ");
+                    int power = int.Parse(Console.ReadLine());
+                    powerOfTwo.CheckPowerOfTwo(power);
                     break;
                 default:
                     Console.WriteLine("Input valid option");
